bc61e2528188f98d3c80d7524b7b7b9c06af1375
Ahmed Behairy <ahmed.behairy@everestminds.com>
2024-02-16 15:12:07 +0400

    Using mixpanel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import ProtectedRoute from './ProtectedRoute';
 import { Routes, Route } from 'react-router-dom';
+import mixpanel from 'mixpanel-browser';
 
 import Ask from './pages/Ask';
 import AppList from './pages/AppsList';
@@ -14,6 +15,17 @@ import Demo from './pages/Demo';
 import Preview from './pages/Preview';
 
 function App() {
+  // Check if environment is production
+  if (process.env.NODE_ENV === 'production') {
+    mixpanel.init("4c8891fc8ba1583180b1cd796e0c8f9c", { debug: false }); // Set debug to false in production
+    mixpanel.track('boot', {
+      environment: 'production'
+    });
+  } else {
+    // Optional: Setup for development (e.g., logging for development)
+    console.log("Mixpanel is disabled in development mode.");
+  }
+
   return (
     <HelmetProvider>
       <Routes>
